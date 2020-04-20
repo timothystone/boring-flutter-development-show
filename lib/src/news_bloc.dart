efd6948daaecb91fa1c20954f2346ae4123279d9
Timothy Stone <github@petmystone.com>
2020-04-20 07:54:02 -0400

    Refactor the New BLoC
    Minor code organization in the file. Refactored `getArticles` to `getArticle`
    Added a BehaviorSubject to the articles stream to provide an _isLoading event that
    surfaces information for an animation in the UI.

--- lib/src/news_bloc.dart
+++ lib/src/news_bloc.dart
@@ -4,47 +4,8 @@ import 'package:boring_flutter_dev/src/articles.dart';
 import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
 
-enum StoriesType {
-  topStories,
-  newStories,
-}
-
 class NewsBloc {
-  /// BehaviorSubject
-  /// Streams are asynchrous, nice but problems exist. build() method is synchronous.
-  /// Changes may present empty.
-  /// BS is the last element pushed into the Stream
-  ///
-  final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
-
-  // empty list of articles
-  var _articles = <Article>[];
-
-  Sink<StoriesType> get storiesType => _storiesTypeController.sink;
-
-  final _storiesTypeController = StreamController<StoriesType>();
-
-  NewsBloc() {
-    _getArticlesAndUpdate(_topIds);
-
-    _storiesTypeController.stream.listen((storiesType) {
-      if(storiesType == StoriesType.newStories) {
-        _getArticlesAndUpdate(_newIds);
-      } else {
-        _getArticlesAndUpdate(_topIds);
-      }
-    });
-  }
-
-  _getArticlesAndUpdate(List<int> ids) {
-    _updateArticles(ids).then((_) {
-      _articlesSubject.add(UnmodifiableListView(_articles));
-    });
-  }
-
-  Stream<UnmodifiableListView<Article>> get articles => _articlesSubject.stream;
-
-   //  List<Article> _articles = []; //articles;
+  //  List<Article> _articles = []; //articles;
   static List<int> _newIds = [
     22867627,
     22871331,
@@ -61,7 +22,46 @@ class NewsBloc {
     22870905,
   ];
 
-  Future<Article> _getArticles(int storyId) async {
+  Stream<bool> get isLoading => _isLoadingSubject.stream;
+
+  final _isLoadingSubject = BehaviorSubject<bool>.seeded(false);
+
+  /// BehaviorSubject
+  /// Streams are asynchrous, nice but problems exist. build() method is synchronous.
+  /// Changes may present empty.
+  /// BS is the last element pushed into the Stream
+  final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
+
+  // empty list of articles
+  var _articles = <Article>[];
+
+  final _storiesTypeController = StreamController<StoriesType>();
+
+  NewsBloc() {
+    _getArticlesAndUpdate(_topIds);
+
+    _storiesTypeController.stream.listen((storiesType) {
+      if(storiesType == StoriesType.newStories) {
+        _getArticlesAndUpdate(_newIds);
+      } else {
+        _getArticlesAndUpdate(_topIds);
+      }
+    });
+  }
+
+  _getArticlesAndUpdate(List<int> ids) async {
+    _isLoadingSubject.add(true);
+    _updateArticles(ids).then((_) {
+      _articlesSubject.add(UnmodifiableListView(_articles));
+      _isLoadingSubject.add(false);
+    });
+  }
+
+  Stream<UnmodifiableListView<Article>> get articles => _articlesSubject.stream;
+
+  Sink<StoriesType> get storiesType => _storiesTypeController.sink;
+
+  Future<Article> _getArticle(int storyId) async {
     final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$storyId.json';
     final storyResponse = await http.get(storyUrl);
     if (storyResponse.statusCode == 200) {
@@ -70,9 +70,13 @@ class NewsBloc {
   }
 
   Future<Null> _updateArticles(List<int> articleIds) async {
-    final futureArticles = articleIds.map((id) => _getArticles(id));
+    final futureArticles = articleIds.map((id) => _getArticle(id));
     final articles = await Future.wait(futureArticles);
     _articles = articles;
   }
+}
 
+enum StoriesType {
+  topStories,
+  newStories,
 }
